118912b26f921584f2ca7a7ab7585893af5864bf
nammm <108422464+Criezzz@users.noreply.github.com>
2025-12-07 16:48:51 +0700

    Update firebase.js

--- configs/firebase.js
+++ configs/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUWRBa5k8V9-Y9Akaciq_9K7iZFeROrRw",
-  authDomain: "webapp-69eb5.firebaseapp.com",
-  projectId: "webapp-69eb5",
-  storageBucket: "webapp-69eb5.firebasestorage.app",
-  messagingSenderId: "707131182426",
-  appId: "1:707131182426:web:bad5c901add96ff65ef763"
+  apiKey: "AIzaSyBpKTmFPk2UPve75gLSZ2zOtkjwJ4lD5Ng",
+  authDomain: "device-streaming-443f3261.firebaseapp.com",
+  projectId: "device-streaming-443f3261",
+  storageBucket: "device-streaming-443f3261.firebasestorage.app",
+  messagingSenderId: "196498253672",
+  appId: "1:196498253672:web:64d9949756993d841a18dd"
 };
 
 export default firebaseConfig;
